16775a336c7da3b94f3754d64705507b33fde6bc
David Marsh <rdmarsh@gmail.com>
2013-08-16 00:15:47 +1000

    getting routing working

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Mdl::Application.routes.draw do
+  root 'organisations#index'
+  get "organisations/index"
   resources :organisations
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
