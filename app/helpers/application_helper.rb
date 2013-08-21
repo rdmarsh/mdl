84bbe29e509e9d4dd7d51d54d3bb8c0329fef6c4
David Marsh <rdmarsh@gmail.com>
2013-08-22 00:51:42 +1000

    Playing with footers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "MDL"
+    base_title = "onemdl"
     if page_title.empty?
       base_title
     else
