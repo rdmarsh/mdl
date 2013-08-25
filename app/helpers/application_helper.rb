cd41969411eead45540d78c811b2b66985a89710
David Marsh <rdmarsh@gmail.com>
2013-08-25 23:51:05 +1000

    change onemdl to omdl to save space

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "onemdl"
+    base_title = "omdl"
     if page_title.empty?
       base_title
     else
