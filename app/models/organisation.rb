33735d029999d8fdc477b51a9c6dae7c7f3f7ba8
David Marsh <rdmarsh@gmail.com>
2013-09-02 09:55:36 +1000

    only let and num referenc

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -1,6 +1,8 @@
 class Organisation < ActiveRecord::Base
   before_save { self.reference = reference.downcase }
   
+  validates_format_of :reference, :with => /\A[-a-z]+\z/, :message => "can only contain letters and numbers."
+  
   validates :reference, presence: true
   validates :name, presence: true
   validates :supportlevel, presence: true
