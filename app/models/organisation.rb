162ad01d6bccb8056f16c914b948882bf323bd84
David Marsh <rdmarsh@gmail.com>
2013-09-02 12:43:09 +1000

    fixed reference field check

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -1,7 +1,7 @@
 class Organisation < ActiveRecord::Base
   before_save { self.reference = reference.downcase }
   
-  validates_format_of :reference, :with => /\A[-a-z]+\z/, :message => "can only contain letters and numbers."
+  validates_format_of :reference, :with => /\A[a-zA-Z\d]+\z/, :message => "Can only contain letters and numbers."
   
   validates :reference, presence: true
   validates :name, presence: true
