d106a85df269f081b81e817207af0744471f717e
David Marsh <rdmarsh@gmail.com>
2013-09-09 09:55:23 +1000

    cleaned up org show form

--- app/controllers/organisations_controller.rb
+++ app/controllers/organisations_controller.rb
@@ -52,7 +52,7 @@ class OrganisationsController < ApplicationController
   end
   
   def sort_column
-    Organisation.column_names.include?(params[:sort]) ? params[:sort] : "id"
+    Organisation.column_names.include?(params[:sort]) ? params[:sort] : "reference"
   end
   
   
