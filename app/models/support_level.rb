87ef33ab5cd6a88a0daa012c0a8482d90cc0b7e7
David Marsh <rdmarsh@gmail.com>
2013-09-10 09:53:36 +1000

    wip fixing edit show tabs

--- app/models/support_level.rb
+++ app/models/support_level.rb
@@ -1,6 +1,8 @@
 class SupportLevel < ActiveRecord::Base
   has_many :organisations
   
+  before_save { self.name = name.humanize }
+  
   validates_presence_of :name
   validates_presence_of :hours
   validates_presence_of :days
