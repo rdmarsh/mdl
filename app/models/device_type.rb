b06a75da4a1c088e46c76d67a04e9272fe34fd32
David Marsh <rdmarsh@gmail.com>
2013-09-13 19:00:48 +1000

    working on validations

--- app/models/device_type.rb
+++ app/models/device_type.rb
@@ -1,9 +1,11 @@
 class DeviceType < ActiveRecord::Base
   has_many :device_models
+  validates_associated :device_models
   
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
   
   # before_save { self.name = name.downcase }
+  # validates_uniqueness_of :name, 
   
   before_destroy :protect_unknown
   
