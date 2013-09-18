f78460f74e12a97604f75d454f7e24dac0cff483
David Marsh <rdmarsh@gmail.com>
2013-09-18 22:10:12 +1000

    starting work on 'can destroy'

--- app/models/device_model.rb
+++ app/models/device_model.rb
@@ -12,7 +12,7 @@ class DeviceModel < ActiveRecord::Base
   
   
   # has many
-  has_many :devices
+  has_many :devices, dependent: :restrict_with_error
   
   # validations
   validates :name, presence: true, uniqueness: { case_sensitive: false }
