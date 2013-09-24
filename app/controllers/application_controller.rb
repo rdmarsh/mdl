0cd687883ad16d81da052cf14b6cdf627e0b2186
David Marsh <rdmarsh@gmail.com>
2013-09-24 11:35:26 +1000

    added record not found exection for unknow userids

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,7 @@ class ApplicationController < ActionController::Base
   
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    rescue ActiveRecord::RecordNotFound
   end
   helper_method :current_user
   hide_action :current_user
