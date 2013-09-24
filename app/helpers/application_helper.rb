8eb2d1db4af159dcb26c3eeba6dbc62280495665
David Marsh <rdmarsh@gmail.com>
2013-09-25 07:40:05 +1000

    made size a var

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -68,11 +68,13 @@ module ApplicationHelper
   
   # gravatar avatar helper, from railscasts ep 244
   def avatar_url(user)
+    size=48
+    
     if user.avatar_url.present?
       user.avatar_url
     else
       gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-      "http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=wavatar"
+      "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=wavatar"
     end
   end
 end
