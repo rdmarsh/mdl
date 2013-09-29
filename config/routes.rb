b0b0d7c9a687041dd15dd6c7b30ef5e3f36ef454
David Marsh <rdmarsh@gmail.com>
2013-09-29 21:47:14 +1000

    work on routes

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,12 @@ Onemdl::Application.routes.draw do
   resources :announcements
   match 'announcements/:id/hide', to: 'announcements#hide', as: 'hide_announcement', via: :get
   
+  resources :organisations do
+    resources :relationships
+    resources :devices
+  end
+  
+  resources :relationships
   
   resources :devices do
     resources :support_levels
@@ -25,16 +31,9 @@ Onemdl::Application.routes.draw do
     resources :licenses
   end
   
-  
-  resources :license_types do
-    resources :organisations
-  end
-  
-  resources :licenses do
-    resources :license_types
-  end
-  
-  resources :license_types
+  resources :support_levels
+  resources :service_levels
+  resources :environments
   
   resources :network_interfaces do
     resources :network_uses
@@ -42,16 +41,14 @@ Onemdl::Application.routes.draw do
   
   resources :network_uses
   
-  resources :organisations do
-    resources :relationships
-    resources :devices
+  resources :licenses do
+    resources :license_types
   end
-  resources :devices
   
-  resources :support_levels
-  resources :service_levels
-  resources :relationships
-    
+  resources :license_types do
+    resources :organisations
+  end
+  
   resources :device_models do
     resources :device_types
     resources :manufacturers
@@ -59,8 +56,6 @@ Onemdl::Application.routes.draw do
   
   resources :device_types
   resources :manufacturers
-  
-  resources :environments
   
   root 'staticpages#home'
   
@@ -73,60 +68,4 @@ Onemdl::Application.routes.draw do
   match '/about',   to: 'staticpages#about',    via: 'get'
   match '/contact', to: 'staticpages#contact',  via: 'get'
   match '/faq',     to: 'staticpages#faq',      via: 'get'
-  
-  
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-  
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
