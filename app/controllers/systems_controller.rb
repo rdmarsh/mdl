1fd4a7d79b021d892fae045987a287c6c86cde72
David Marsh <rdmarsh@gmail.com>
2013-10-05 13:04:08 +1000

    update routes for pagination, and add minor about text

--- app/controllers/systems_controller.rb
+++ app/controllers/systems_controller.rb
@@ -1,78 +1,11 @@
-class SystemsController < ApplicationController
+class SystemsController < InheritedResources::Base
   respond_to :html, :xml, :json, :csv
-  helper_method :sort_column, :sort_direction
   
-  def index
-    @organisation.present? ? @organisation = Organisation.friendly.find(params[:organisation_id]) : nil
-    @systems = @organisation.present? ? @organisation.systems.page(params[:page]) : System.page(params[:page])
-  end
+  belongs_to :organisation, :optional => true
   
-  def show
-    @system = System.friendly.find(params[:id])
-    respond_with(@system)
-  end
+  protected
   
-  def new
-    @system = System.new
-    respond_with(@system)
-  end
-  
-  def create
-    @system = System.new(system_params)
-    
-    if @system.save
-      # write a create message to the activity log
-      @system.create_activity :create, owner: current_user
-      flash[:info] = "Successfully created system '" + @system.name + "'"
-    else
-      flash[:error] = "Could not create system '" + @system.name + "'"
-    end
-    
-    respond_with(@system)
-  end
-  
-  def edit
-    @system = System.friendly.find(params[:id])
-    respond_with(@system)
-  end
-  
-  def update
-    @system = System.friendly.find(params[:id])
-    
-    if @system.update_attributes(system_params)
-      # write an update message to the activity log
-      @system.create_activity :update, owner: current_user
-      flash[:info] = "Successfully updated system '" + @system.name + "'"
-    else
-      flash[:error] = "Could not update system '" + @system.name + "'"
-    end
-
-    respond_with(@system)
-  end
-  
-  def destroy
-    @system = System.friendly.find(params[:id])
-    
-    # write an update message to the activity log, it fails with a "cannot call create unless the parent is saved" inside the if statement, so I've put it here. It works out anyway, as we can show both successes and failures
-    @system.create_activity :destroy, owner: current_user
-    if @system.destroy
-      flash[:info] = "Successfully deleted system '" + @system.name + "'"
-    else
-      flash[:error] = "Could not delete system '" + @system.name + "'"
-    end
-    
-    respond_with(@system)
-  end
-  
-  private
-  
-  # for sorting columns
-  def sort_column
-    System.column_names.include?(params[:sort]) ? params[:sort] : "id"
-  end
-  
-  # for allowing editing on fields
-  def system_params
-    params.require(:system).permit!
+  def collection
+    @systems ||= end_of_association_chain.page(params[:page])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Onemdl::Application.routes.draw do
+  # pagination MUST be above nested resources or URLs will break
+  
+  # routes home to root
+  root 'staticpages#home'
+  
+  # static pages routing
+  match '/help',    to: 'staticpages#help',     via: 'get'
+  match '/about',   to: 'staticpages#about',    via: 'get'
+  match '/contact', to: 'staticpages#contact',  via: 'get'
+  match '/faq',     to: 'staticpages#faq',      via: 'get'
   
   # routes for signup, login and logout pages
   get 'signup', to: 'users#new', as: 'signup'
@@ -7,81 +17,176 @@ Onemdl::Application.routes.draw do
   
   # storing user sessions
   resources :sessions
-  resources :users
-  
-  resources :statistics
-  resources :activities
   
   # site wide announcements
   resources :announcements
   match 'announcements/:id/hide', to: 'announcements#hide', as: 'hide_announcement', via: :get
   
-  resources :systems do
-    resources :network_interfaces
+  # statistics
+  resources :statistics
+  
+  ##############################################################################
+  # :activities
+  resources :activities do
+    get 'page/:page', :action => :index, :on => :collection
   end
   
-  resources :device_models do
-    resources :systems
+  ##############################################################################
+  # :users and nested resources
+  resources :users do
+    get 'page/:page', :action => :index, :on => :collection
   end
   
-  resources :device_types do
-    resources :device_models
-  end
-  
-  resources :environments do
-    resources :systems
-  end
-  
-  resources :license_types do
-    resources :licenses
-  end
-  
-  resources :licenses do
-    resources :systems
-  end
-  
+  ##############################################################################
+  # :manufacturers and nested resources
   resources :manufacturers do
-    resources :device_models
-  end
-  
-  resources :network_interfaces
-  
-  resources :network_uses do
-    resources :network_interfaces
-  end
-  
-  resources :operating_systems do
-    resources :systems
-  end
-  
-  resources :organisations do
-    resources :license_types
-    resources :licenses
-    resources :operating_systems
-    resources :systems
-  end
-  
-  resources :relationships do
-    resources :organisations
-  end
-  
-  resources :service_levels do
-    resources :systems
-  end
-  
-  resources :support_levels do
-    resources :systems
-  end
+    get 'page/:page', :action => :index, :on => :collection
     
-  root 'staticpages#home'
+    resources :device_models do
+      get 'page/:page', :action => :index, :on => :collection
+    end
+  end
+  
+  ##############################################################################
+  # :device_types and nested resources
+  resources :device_types do
+    get 'page/:page', :action => :index, :on => :collection
+    
+    resources :device_models do
+      get 'page/:page', :action => :index, :on => :collection
+    end
+  end
+  
+  ##############################################################################
+  # :device_models and nested resources
+  resources :device_models do
+    get 'page/:page', :action => :index, :on => :collection
+    
+    resources :systems do
+      get 'page/:page', :action => :index, :on => :collection
+    end
+  end
+  
+  ##############################################################################
+  # :environments and nested resources
+  resources :environments do
+    get 'page/:page', :action => :index, :on => :collection
+    
+    resources :systems do
+      get 'page/:page', :action => :index, :on => :collection
+    end
+  end
+  
+  ##############################################################################
+  # :service_levels and nested resources
+  resources :service_levels do
+    get 'page/:page', :action => :index, :on => :collection
+    
+    resources :systems do
+      get 'page/:page', :action => :index, :on => :collection
+    end
+  end
+  
+  ##############################################################################
+  # :support_levels and nested resources
+  resources :support_levels do
+    get 'page/:page', :action => :index, :on => :collection
+    
+    resources :systems do
+      get 'page/:page', :action => :index, :on => :collection
+    end
+  end
+  
+  ##############################################################################
+  # :relationships and nested resources
+  resources :relationships do
+    get 'page/:page', :action => :index, :on => :collection
+    
+    resources :organisations do
+      get 'page/:page', :action => :index, :on => :collection
+    end
+  end
+  
+  ##############################################################################
+  # :organisations and nested resources
+  resources :organisations do
+    get 'page/:page', :action => :index, :on => :collection
+    
+    resources :license_types do
+      get 'page/:page', :action => :index, :on => :collection
+    end
+    
+    resources :licenses do
+      get 'page/:page', :action => :index, :on => :collection
+    end
+    
+    resources :operating_systems do
+      get 'page/:page', :action => :index, :on => :collection
+    end
+    
+    resources :systems do
+      get 'page/:page', :action => :index, :on => :collection
+    end
+  end
+
+  ##############################################################################
+  # :license_types and nested resources
+  resources :license_types do
+    get 'page/:page', :action => :index, :on => :collection
+    
+    resources :licenses do
+      get 'page/:page', :action => :index, :on => :collection
+    end
+  end
+  
+  ##############################################################################
+  # :licenses and nested resources
+  resources :licenses do
+    get 'page/:page', :action => :index, :on => :collection
+    
+    resources :systems do
+      get 'page/:page', :action => :index, :on => :collection
+    end
+  end
+  
+  ##############################################################################
+  # :operating_systems and nested resources
+  resources :operating_systems do
+    get 'page/:page', :action => :index, :on => :collection
+    
+    resources :systems do
+      get 'page/:page', :action => :index, :on => :collection
+    end
+  end
+  
+  ##############################################################################
+  # :systems and nested resources
+  resources :systems do
+    get 'page/:page', :action => :index, :on => :collection
+    
+    resources :network_interfaces do
+      get 'page/:page', :action => :index, :on => :collection
+    end
+  end
+  
+  ##############################################################################
+  # :network_uses and nested resources
+  resources :network_uses do
+    get 'page/:page', :action => :index, :on => :collection
+
+    resources :network_interfaces do
+      get 'page/:page', :action => :index, :on => :collection
+    end
+  end
+  
+  ##############################################################################
+  # :network_interfaces
+  resources :network_interfaces do
+    get 'page/:page', :action => :index, :on => :collection
+  end
   
   resources :onemdl_settings, :only => [:index, :edit]
   
-  match '/onemdl_settings',      to: 'onemdl_settings#index',     via: 'get'
+  match '/onemdl_settings',      to: 'onemdl_settings#index',    via: 'get'
   match '/onemdl_settings/edit', to: 'onemdl_settings#edit',     via: 'get'
-  
-  match '/help',    to: 'staticpages#help',     via: 'get'
-  match '/about',   to: 'staticpages#about',    via: 'get'
-  match '/contact', to: 'staticpages#contact',  via: 'get'
-  match '/faq',     to: 'staticpages#faq',      via: 'get'
 end
