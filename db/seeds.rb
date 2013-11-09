51dbe5e8df561f5a5c367f7fe03c03006bd0a16a
David Marsh <rdmarsh@gmail.com>
2013-11-09 20:02:00 +1100

    comments in seed and lic

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+# OneMDL is a ruby on rails based application designed to collate information
+# about IT assets. It is designed to be most useful to System Administrators
+# and Service Desk personal.
+# 
+# Copyright (C) 2013 David Marsh
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# This file contains the records to seed the database with default values.
+# The data can then be loaded with the rake db:seed
+# (or created alongside the db with db:setup).
 #
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 
 # Address.create(name: "None")
 # Address.create(name: "Unknown")
